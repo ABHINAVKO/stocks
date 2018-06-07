2033923ec20dc14890027b39a8fd95ff7aa983e4
ABHINAV <31447285+ABHINAVKO@users.noreply.github.com>
2018-06-07 17:23:34 -0500

    Initial commit

---
+++ companies/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.views import  APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Stock
+from .serializers import StockSerializer
+from django.http import Http404
+
+# Create your views here.
+#list all stocks or create a new one
+#stocks/  or stocks/AMZN
+class StockList(APIView):
+
+    def get(self,request):
+        stocks=Stock.objects.all()
+        serializer=StockSerializer(stocks,many=True)
+        return Response(serializer.data)
+
+    def post(self,request):
+        serializer=StockSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status.HTTP_201_CREATED)
+        return Response(serializer.data,status.HTTP_400_BAD_REQUEST)
+
+
+class StockDetail(APIView):
+    def get_object(self,pk):
+        try:
+            return Stock.objects.get(pk=pk)
+        except Stock.DoesNotExist:
+            raise Http404
+
+    def get(self,request,pk):
+        obj=self.get_object(pk)
+        serializer=StockSerializer(obj)
+        return Response(serializer.data)
+
+    def put(self,request,pk):
+        obj=self.get_object(pk)
+        serializer=StockSerializer(obj,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.data,status.HTTP_400_BAD_REQUEST)
+
+    def delete(self,request,pk):
+        obj=self.get_object(pk)
+        obj.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
